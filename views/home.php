da9c1f9385a4198e632f5bc68520a9a49a13a5f3
Ezechiel Batchi <ezechielbatchi@MacBook-Pro-de-Ezechiel.local>
2024-11-30 12:11:45 +0100

    Mise à jour du CSS et centrage de la machine à sous

---
+++ views/home.php
@@ -0,0 +1,18 @@
+<?php
+$headTitle = "Accueil";
+ob_start();
+?>
+
+  <section class="main-sections">
+    <article class="main-articles">
+      <h1 class="main-articles-title">
+        Bienvenue sur Blog Voyage
+      </h1>
+      <p>
+       Bienvenue AU CASINO ROYAL, ici c'est soit tu gagnes, soit tu gaaaaaagnes💰💰💰💰💰💰
+      </p>
+    </article>
+  </section>
+
+<?php
+$mainContent = ob_get_clean();
